367495efb2d09e520a94a9dd69c05bbe2802bba3
danostpav <danostpav@gmail.com>
2021-06-14 22:19:26 +0300

    adding cases

--- scripts/loadData.js
+++ scripts/loadData.js
@@ -11,8 +11,11 @@ loadData()
     .then(() => {
         let preloaderBM = document.getElementById('bm');
         let preloaderEl = document.getElementById('preloader');
+
         preloaderEl.classList.add('hidden');
         preloaderEl.classList.remove('visible');
         preloaderBM.classList.add('hidden');
         preloaderBM.classList.remove('vidible');
+
     });
+
---
+++ scripts/loadDataCase.js
@@ -0,0 +1,1 @@
+window.onload = function(){ document.querySelector(".preloader_case").style.display = "none"; }
