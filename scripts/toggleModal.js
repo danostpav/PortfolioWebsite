de8f5eb63c79c9a1ddf3d8b27373be8e87c0857f
danostpav <danostpav@gmail.com>
2021-06-15 23:31:31 +0300

    zavr bog

--- scripts/toggleModal.js
+++ scripts/toggleModal.js
@@ -1,4 +1,7 @@
 function toggleModal(event) {
+    if (event.querySelector('.modal').style.display == 'block') {
+        return
+    }
             console.log(event);
             var elmnt = event.querySelector('.modal-content div');
             var file = elmnt.getAttribute("w3-include-html");
@@ -7,7 +10,10 @@ function toggleModal(event) {
                 xhttp = new XMLHttpRequest();
                 xhttp.onreadystatechange = function () {
                     if (this.readyState == 4) {
-                        if (this.status == 200) { elmnt.innerHTML = this.responseText; }
+                        if (this.status == 200) { elmnt.innerHTML = this.responseText; 
+                        
+                            
+                        }
                         if (this.status == 404) { elmnt.innerHTML = "Page not found."; }
                         /*remove the attribute, and call this function once more:*/
                         elmnt.removeAttribute("w3-include-html");
@@ -18,11 +24,12 @@ function toggleModal(event) {
                 xhttp.send();
                 /*exit the function:*/
                 event.querySelector('.modal').style.display = "block";
+                event.parentNode.querySelector('.modal-overlay').style.display = 'block';
             } else {
+                console.log(event)
                 if (event.querySelector('.modal').style.display == 'none') {
                     event.querySelector('.modal').style.display = 'block';
-                } else {
-                    event.querySelector('.modal').style.display = "none";
+                    event.parentNode.querySelector('.modal-overlay').style.display = 'block';
                 }
 
             }
@@ -30,4 +37,17 @@ function toggleModal(event) {
             
         }
 
-         
+    
+        function closeModal(event) {
+            console.log(event);
+            event.preventDefault();
+            event = event.target;
+
+              event.parentNode.querySelector('.modal').style.display = "none";
+              event.style.display = "none";
+
+        }   
+        
+
+
+       
