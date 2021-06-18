5e761caf248f7d9d0545d1c6a4ebcfe15f311e78
danostpav <danostpav@gmail.com>
2021-06-18 12:53:13 +0300

    double tap

--- scripts/closeModal.js
+++ scripts/closeModal.js
@@ -5,5 +5,22 @@ function closeModal(event) {
 
       event.parentNode.querySelector('.modal').style.display = "none";
       event.style.display = "none";
+      elm1.addEventListener('touchend', function (event) {
+        var currentTime = new Date().getTime();
+        var tapLength = currentTime - lastTap;
+        clearTimeout(timeout);
+        if (tapLength < 500 && tapLength > 0) {
+            event.parentNode.querySelector('.modal').style.display = "none";
+            event.style.display = "none";
+            event.preventDefault();
+        } else {
+            elm2.innerHTML = 'Single Tap';
+            timeout = setTimeout(function () {
+                elm2.innerHTML = 'Single Tap (timeout)';
+                clearTimeout(timeout);
+            }, 500);
+        }
+        lastTap = currentTime;
+    });
 
 }   
