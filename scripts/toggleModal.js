9ae741877c182b238a08d84f65d570db7266048e
danostpav <danostpav@gmail.com>
2021-06-18 12:53:00 +0300

    trying double tap close

--- scripts/toggleModal.js
+++ scripts/toggleModal.js
@@ -2,46 +2,47 @@ function toggleModal(event) {
     if (event.querySelector('.modal').style.display == 'block') {
         return
     }
-            console.log(event);
-            var elmnt = event.querySelector('.modal-content div');
-            var file = elmnt.getAttribute("w3-include-html");
-            if (file) {
-                console.log(file);
-                xhttp = new XMLHttpRequest();
-                xhttp.onreadystatechange = function () {
-                    if (this.readyState == 4) {
-                        if (this.status == 200) { elmnt.innerHTML = this.responseText; 
-                        
-                            
-                        }
-                        if (this.status == 404) { elmnt.innerHTML = "Page not found."; }
-                        /*remove the attribute, and call this function once more:*/
-                        elmnt.removeAttribute("w3-include-html");
-                        //includeHTML();
-                    }
-                }
-                xhttp.open("GET", file, true);
-                xhttp.send();
-                /*exit the function:*/
-                event.querySelector('.modal').style.display = "block";
-                event.parentNode.querySelector('.modal-overlay').style.display = 'block';
-                event.querySelector('.modal-overlay-m').style.display = "block";
-            } else {
-                console.log(event)
-                if (event.querySelector('.modal').style.display == 'none') {
-                    event.querySelector('.modal').style.display = 'block';
-                    event.parentNode.querySelector('.modal-overlay').style.display = 'block';
-                    event.querySelector('.modal-overlay-m').style.display = "block";
-                }
+    console.log(event);
+    var elmnt = event.querySelector('.modal-content div');
+    var file = elmnt.getAttribute("w3-include-html");
+    if (file) {
+        console.log(file);
+        xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function () {
+            if (this.readyState == 4) {
+                if (this.status == 200) {
+                    elmnt.innerHTML = this.responseText;
 
+
+                }
+                if (this.status == 404) { elmnt.innerHTML = "Page not found."; }
+                /*remove the attribute, and call this function once more:*/
+                elmnt.removeAttribute("w3-include-html");
+                //includeHTML();
             }
-
-            
+        }
+        xhttp.open("GET", file, true);
+        xhttp.send();
+        /*exit the function:*/
+        event.querySelector('.modal').style.display = "block";
+        event.parentNode.querySelector('.modal-overlay').style.display = 'block';
+        event.querySelector('.modal-overlay-m').style.display = "block";
+    } else {
+        console.log(event)
+        if (event.querySelector('.modal').style.display == 'none') {
+            event.querySelector('.modal').style.display = 'block';
+            event.parentNode.querySelector('.modal-overlay').style.display = 'block';
+            event.querySelector('.modal-overlay-m').style.display = "block";
         }
 
+    }
+
     
-        
-        
+
+}
 
 
-       
+
+
+
+
